e75aa688ae85e0d9cfc87259393aa0f0f407f8cc
abdula <ihor.bykov@gmail.com>
2026-05-25 00:18:27 +0300

    config changing

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Cybercrime Intelligence",
+    pageTitle: "Threat Intelligence",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ihorbykov.github.io/investigator",
+    baseUrl: "openintel.online",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
